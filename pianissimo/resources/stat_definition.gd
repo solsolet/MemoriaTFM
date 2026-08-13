7cdd0e4934bfe9a3c8d94644d5442e0842cccf0a
solsolet <gsl21@alu.ua.es>
2026-08-14 01:22:39 +0200

    feat: Millores de presició i velocitat

--- pianissimo/resources/stat_definition.gd
+++ pianissimo/resources/stat_definition.gd
@@ -3,6 +3,6 @@ class_name StatDefinition
 
 @export var id: String = ""
 @export var display_name: String = ""
-@export var base_cost: int = 10
+@export var base_cost: int = 50
 @export var cost_multiplier: float = 1.3 # exponencial, més costós que les upgrades
 @export var max_level: int = -1
